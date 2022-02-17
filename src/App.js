ff91de098666977332ff7a798a5a172ab519081e
Ajnur <aradovic@bmcgruop.com>
2022-02-17 22:51:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Eyeball from "./Eyeball";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Canvas clasName="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} />
+        <Suspense fallback={null}>
+          <Eyeball scale={25.01}  position={[1, 1, 2]} />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
